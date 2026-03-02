d3a0ed4e64c8add273d6157e09a39e86a4130efa
eriktedhamre <42960003+eriktedhamre@users.noreply.github.com>
2026-03-02 17:59:44 +0100

    Bery small compiler

--- code/code.go
+++ code/code.go
@@ -1,6 +1,9 @@
 package code
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+)
 
 type Instructions []byte
 
@@ -17,6 +20,35 @@ type Definition struct {
 
 var definitions = map[Opcode]*Definition{
 	OpConstant: {"OpConstant", []int{2}},
+}
+
+func Make(op Opcode, operands ...int) []byte {
+	def, ok := definitions[op]
+
+	if !ok {
+		return []byte{}
+	}
+
+	instructionLen := 1
+
+	for _, w := range def.OperandWidths {
+		instructionLen += w
+	}
+
+	instruction := make([]byte, instructionLen)
+	instruction[0] = byte(op)
+
+	offset := 1
+	for i, o := range operands {
+		width := def.OperandWidths[i]
+		switch width {
+		case 2:
+			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		}
+		offset += width
+	}
+
+	return instruction
 }
 
 func Lookup(op byte) (*Definition, error) {
