c6a27f35a10cac464dc2a37b1330ce5ffbdfad69
eriktedhamre <42960003+eriktedhamre@users.noreply.github.com>
2026-06-07 15:59:48 +0200

    Finished Arrays and Hashes

--- code/code.go
+++ code/code.go
@@ -31,6 +31,7 @@ const (
 	OpSetGlobal
 	OpArray
 	OpHash
+	OpIndex
 )
 
 type Definition struct {
@@ -57,8 +58,9 @@ var definitions = map[Opcode]*Definition{
 	OpNull:          {"OpNull", []int{}},
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
-	OpArray: 		 {"OpArray", []int{2}},
-	OpHash:	{"OpHash", []int{2}},
+	OpArray:         {"OpArray", []int{2}},
+	OpHash:          {"OpHash", []int{2}},
+	OpIndex:         {"OpIndex", []int{}},
 }
 
 func Make(op Opcode, operands ...int) []byte {
